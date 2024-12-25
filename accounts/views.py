a650d86c28adb06de974055321a43b3e4462d7fb
bini <jaletakebede@gmail.com>
2024-12-25 20:01:55 +0300

    keeping local repo upto-date with remote

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def home(request):
+    return render(request, 'home.html')
